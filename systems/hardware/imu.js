6effc1ea8429899f6ce7655f9135d036cedcba51
brudiW <146438483+brudiW@users.noreply.github.com>
2026-02-22 17:56:13 +0100

    Create imu.js

---
+++ systems/hardware/imu.js
@@ -0,0 +1,39 @@
+const imu_event = new BroadcastChannel("imu_event");
+
+export class IMU {
+    constructor() {
+        this.pitch = 90;
+        this.yaw = 213.3;
+        this.roll = -180;
+        this.prate = 0;
+        this.yrate = 0;
+        this.rrate = 0;
+        this.yawMode = "heading"; // "vector"
+        this.lastTime = performance.now();
+        this.update = this.update.bind(this)
+    }
+    update() {
+
+        const now = performance.now();
+        const dt = (now - this.lastTime) / 1000; // seconds
+        this.lastTime = now;
+
+        this.pitch += this.prate * dt;
+        this.roll += this.rrate * dt;
+        this.yaw += this.yrate * dt;
+
+        // Clamp / wrap
+        this.pitch = ((this.pitch + 180) % 360) - 180;
+        this.roll = ((this.roll + 180) % 360) - 180;
+        this.yaw = (this.yaw + 360) % 360;
+        imu_event.postMessage("");
+
+
+        setImmediate(this.update)
+    }
+    setRates(rates) {
+        this.prate = rates[0];
+        this.rrate = rates[1];
+        this.yrate = rates[2];
+    }
+}
