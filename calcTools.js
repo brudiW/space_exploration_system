8dc5c060c7c914801dd36dc6d7f82703b2b85764
brudiW <146438483+brudiW@users.noreply.github.com>
2026-02-23 15:09:38 +0100

    Update calcTools.js
    Refactor CoordsToGroundVector to correct x and z calculations. Add groundVectorToLocalFacingVector function for local facing vector calculation.

--- calcTools.js
+++ calcTools.js
@@ -2,9 +2,11 @@ const rEarth = 6378137;
 export function CoordsToGroundVector(lat, lon) {
   lat = lat / 180 * Math.PI;
   lon = lon / 180 * Math.PI;
-  let x = rEarth * Math.sin(lat) * Math.cos(lon);
+  
+  let x = rEarth * Math.cos(lat) * Math.cos(lon);
   let y = rEarth * Math.cos(lat) * Math.sin(lon);
-  let z = rEarth * Math.cos(lat);
+  let z = rEarth * Math.sin(lat);
+  
   return [x, y, z];
 }
 export function groundVectorToCoords(x, y, z) {
@@ -17,4 +19,27 @@ export function groundVectorToLocationVector(x, y, z, h) {
   y = (y / rEarth) * (rEarth + h);
   z = (z / rEarth) * (rEarth + h);
   return [x, y, z];
+}
+export function groundVectorToLocalFacingVector(x, y, z) {
+
+  const R = Math.sqrt(x*x + y*y + z*z);
+
+  // Up-Vektor (normalisiert)
+  const ux = x / R;
+  const uy = y / R;
+  const uz = z / R;
+
+  // Sonderfall Pol
+  const horizontalLen = Math.sqrt(x*x + y*y);
+
+  if (horizontalLen < 1e-6) {
+    return [1, 0, 0]; // beliebige Richtung
+  }
+
+  // Tangentialer Vektor (Ost-Richtung)
+  const kx = -y / horizontalLen;
+  const ky =  x / horizontalLen;
+  const kz =  0;
+
+  return [kx, ky, kz];
 }
