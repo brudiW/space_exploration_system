d7ec3187314f84a54a7bd4bd273107ac4b4a462c
brudiW <146438483+brudiW@users.noreply.github.com>
2026-02-23 10:47:42 +0100

    Create calcTools.js

---
+++ calcTools.js
@@ -0,0 +1,14 @@
+const rEarth = 6378137;
+export function CoordsToGroundVector(lat, lon) {
+  lat = lat / 180 * Math.PI;
+  lon = lon / 180 * Math.PI;
+  let x = rEarth * Math.sin(lat) * Math.cos(lon);
+  let y = rEarth * Math.cos(lat) * Math.sin(lon);
+  let z = rEarth * Math.cos(lat);
+  return [x, y, z];
+}
+export function groundVectorToCoords(x, y, z) {
+  const lat = Math.asin(z / rEarth);
+  const lon = Math.atan2(y, x);
+  return [ lat, lon ];
+}
