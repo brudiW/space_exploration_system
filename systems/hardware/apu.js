5e6465b49277e5f761005bd72df4f455ddf2e50d
brudiW <146438483+brudiW@users.noreply.github.com>
2026-03-17 11:09:28 +0100

    apu.js aktualisieren

--- systems/hardware/apu.js
+++ systems/hardware/apu.js
@@ -3,17 +3,17 @@ export class APU {
         this.id = id;
         this.state = "off"; //"ready", "on", "fail"
         this.autoStartEnabble = false;
-        this.speedPerc = 0.0;
-        this.fuelQty = 100.0;
-        this.fuelPress = 120.0;
-        this.fuelTemp = 70.0;
-        this.h2oQty = 100.0;
-        this.h2oPress = 4.0;
-        this.h2oTemp = 25.0;
-        this.oilQty = 100.0;
-        this.oilPress = 22.5;
-        this.oilTemp = 60.0;
-        this.apuTemp = 45.0;
+        this.speedPerc = 0.0; // %
+        this.fuelQty = 100.0; // %
+        this.fuelPress = 2516586.0; // Pa
+        this.fuelTemp = 70.0; // °C
+        this.h2oQty = 100.0; // %
+        this.h2oPress = 4.0; // Pa
+        this.h2oTemp = 25.0; // °C
+        this.oilQty = 100.0; // %
+        this.oilPress = 22.5; // Pa
+        this.oilTemp = 60.0; // °C
+        this.apuTemp = 45.0; // °C
 
 
 
