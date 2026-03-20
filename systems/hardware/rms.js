aacc07631eda162d456bebfae37a3a8c492e46a0
brudiW <146438483+brudiW@users.noreply.github.com>
2026-03-20 12:14:25 +0100

    rms.js aktualisieren

---
+++ systems/hardware/rms.js
@@ -0,0 +1,10 @@
+export class RMS {
+	constructor() {
+    	this.shoulderPitch = 0;
+        this.shoulderYaw = 0;
+        this.elbowPitch = 0;
+        this.wristPitch = 0;
+        this.wristYaw = 0;
+        this.wristRoll = 0;
+    }
+}
