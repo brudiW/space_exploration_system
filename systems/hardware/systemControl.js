0513bc5b4d04a2384ee6fdce0db440564ba1ae5f
brudiW <146438483+brudiW@users.noreply.github.com>
2026-03-23 21:15:52 +0100

    Add files via upload

---
+++ systems/hardware/systemControl.js
@@ -0,0 +1,11 @@
+// DAS HIER IST DER ÜBERWACHUNGS-"ALGORYTHMUS"
+export function systemControl(OV, opsMode = "102") {
+    // GPC Fault check
+    switch(opsMode) {
+        case "101":
+            break;
+        case "102":
+            
+
+    }
+}
