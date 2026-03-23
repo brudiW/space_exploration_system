05737849d19e3c574979499c665168de65471cbe
brudiW <146438483+brudiW@users.noreply.github.com>
2026-03-23 21:17:13 +0100

    Update rms.js

--- systems/hardware/rms.js
+++ systems/hardware/rms.js
@@ -1,7 +1,7 @@
 export class RMS {
 	constructor() {
-    	this.shoulderPitch = 0;
-        this.shoulderYaw = 0;
+    	this.shoulderPitch = 90;
+        this.shoulderYaw = -90;
         this.elbowPitch = 0;
         this.wristPitch = 0;
         this.wristYaw = 0;
